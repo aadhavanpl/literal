c278c89b8179c611f826d4278318cf381946c04d
AadhavanPL <aadhavanlenin@gmail.com>
2023-01-13 17:59:36 +0530

    removed next files

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css'
-import '../styles/variables.css'
-import '../styles/media-queries.css'
-
-function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />
-}
-
-export default MyApp
