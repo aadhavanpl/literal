8c48ed6bfb2481aea513dad2c44d6e8257533752
AadhavanPL <aadhavanlenin@gmail.com>
2023-01-17 13:08:31 +0530

    file clean

--- website/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template #files with all the urls#
-
-views = Blueprint('views', __name__)
-
-@views.route('/')
-def home():
-    return render_template('home.html', passed="This is passed from another page")
